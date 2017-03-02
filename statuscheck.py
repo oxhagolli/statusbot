54dd37376ef267f2dc4fb26c58eb94b78b1716c3
Orens Xhagolli <orens_xhagolli@intuit.com>
2017-03-01 21:41:41 -0800

    Ping domain and return response.

--- statuscheck.py
+++ statuscheck.py
@@ -1,4 +1,18 @@
+import platform
+import os
 
 
 def status(message):
-    return message
+    host = message[0]
+    if host == "localhost" or host == "127.0.0.1":
+        return "Don't even try ..."
+    host = host.replace('<', '').replace('>', '')
+    if len(host.split("|")) > 1:
+        host = host.split("|")[1]
+    query = "ping {} {}".format(
+        '-n 1' if platform.system().lower() == "windows" else '-c 1',
+        host
+    )
+    print(query)
+    response = "{} is {}.".format(host, "up" if os.system(query) == 0 else "down or does not exist.")
+    return response
